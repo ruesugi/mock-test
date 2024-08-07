814efeb61aa4d66c29bb6abff097e66ad65af006
r_uesugi <r_uesugi@audio-technica.co.jp>
2024-08-07 14:06:50 +0900

    feat:add4を追加.

---
+++ source/add4.c
@@ -0,0 +1,15 @@
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include "add4.h"
+
+#include "add.h"
+#ifdef __cplusplus
+}
+#endif
+
+uint8_t Add4(uint8_t a, uint8_t b)
+{
+    return Add(a, b) + 4;
+}
---
+++ source/add4.h
@@ -0,0 +1,8 @@
+#ifndef ADD4_H
+#define ADD4_H
+
+#include <stdint.h>
+
+uint8_t Add4(uint8_t a, uint8_t b);
+
+#endif  // ADD4_H
